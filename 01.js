4adc483bd856e13c9b8dca1f1b4fab5895e91ddb
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-03 12:04:50 -0600

    add js

--- 01.js
+++ 01.js
@@ -2,3 +2,29 @@
 var producto = 'Audfonos Gamer'; // Iniciar variable y asignarle valor
 
 var disponible; // Iniciamos la varible pero sin valor 
+
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categora = 'Computadoras';// Asignar multiples variables
+/*
+var 1producto; // No se puede iniciar una variable con un numero
+var -producto; // No se puede iniciar una variable con un guion
+var _producto; // Se puede iniciar una variable con un guion bajo
+var $producto; // Se puede iniciar una variable con un signo de pesos   
+*/
+
+var _producto = 'Audifonos'; // No se recomienda iniciar una variable con un guion bajo
+var $producto = 'Teclado'; // No se recomienda iniciar una variable con un signo de pesos
+var _producto = 'Audifonos'; // No se recomienda iniciar una variable con un guion bajo y se puede reasignar el valor
+var $producto = 'Teclado'; // No se recomienda iniciar una variable con un signo de pesos y se puede reasignar el valor
+// console.log(_producto);  // No se recomienda iniciar una variable con un guion bajo
+
+//Estilos de variables 
+var nombre_producto = 'Monitor HD'; //Underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; //Pascal Case    
