d95d0795e486658caf35f42f2cc89022b3e15671
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-04 09:17:54 -0600

    Readme

---
+++ 02.js
@@ -0,0 +1,21 @@
+// variables con let
+// let producto = 'Audfonos Gamer'; // Iniciar variable y asignarle valor
+//
+// let disponible; // Iniciamos la varible pero sin valor
+//
+//
+// producto = true; // Reasignando el valor de la variable
+//
+// disponible = true;
+//      // let producto1 = 'Computadora',
+//      //     disponible1 = true,
+//      //     categora = 'Computadoras';// Asignar multiples variables
+// /*
+// let 1producto; // No se puede iniciar una variable con un numero
+// let -producto; // No se puede iniciar una variable con un guion
+// let _producto; // Se puede iniciar una variable con un guion bajo
+// let $producto; // Se puede iniciar una variable con un signo de pesos
+// */
+//
+// let _producto = 'Audifonos'; // No se recomienda iniciar una variable con un guion bajo
+// let $producto = 'Teclado'; // No se recomienda iniciar una variable con un signo de pesos        
---
+++ 04.js
@@ -0,0 +1,9 @@
+//String o Cadena de Texto ..
+
+const producto = "Monitor de 20\"";
+const producto2 = String('Monitor de 30 pulgadas');
+const producto3 = new String('Monitor de 40 pulgadas');
+
+console.log(producto);
+console.log(typeof producto2);
+console.log(typeof producto3);
