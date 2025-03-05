059be069db1cfcd433488b63fefa27870eaafc96
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-05 12:02:34 -0600

    add news

---
+++ JS/05.js
@@ -0,0 +1,10 @@
+// Numeros 
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 7.57;
+const numero4 = -3;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
