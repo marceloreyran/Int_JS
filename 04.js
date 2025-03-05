27b26a06fb38c04fad61d9ab74468109083fe7d8
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-05 11:32:46 -0600

    js strings

--- 04.js
+++ 04.js
@@ -1,9 +1,18 @@
 //String o Cadena de Texto ..
 
-const producto = "Monitor de 20\"";
+const tweet = "Aprende Javascript con Marcelo";
 const producto2 = String('Monitor de 30 pulgadas');
 const producto3 = new String('Monitor de 40 pulgadas');
+const correo = "correo@correo.com";
 
-console.log(producto);
-console.log(typeof producto2);
-console.log(typeof producto3);
+//lenght se usa para conocer la cantidad de caracteres en una cadena de texto
+console.log(tweet.length);
+
+//Conocer la posicion de un caracter en una cadena de texto
+console.log(tweet.indexOf('Marcelo'));
+// Si no encuentra el caracter en la cadena de texto, devuelve -1
+console.log(producto2.indexOf('javascript'));
+console.log(correo.indexOf('@'));
+//Conocer si un texto esta en una cadena de texto es mas acutal que indexOf
+console.log(tweet.includes('Marcelo'));
+console.log(producto2.includes('javascript'));
