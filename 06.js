a509f06529b829dd479c39a7536af311c79b566f
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-07 09:43:01 -0600

    add info

--- 06.js
+++ 06.js
@@ -6,3 +6,7 @@ resultado = Math.round(2.8);//Redondea
 resultado = Math.ceil(2.49); // Redondea hacia arriba
 resultado = Math.floor(2.99); // Redondea hacia abajo
 console.log(resultado);
+
+// Raiz cuadrada
+resultado = Math.sqrt(144);
+console.log(resultado);
