4256813427e9566477a6ad67cd7572e0814638c7
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-07 09:41:41 -0600

    add change

---
+++ 05.js
@@ -0,0 +1,15 @@
+// Numeros 
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 7.57;
+const numero4 = -3;
+
+// Operaciones
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero1 % numero2);
+console.log(numero1 ** numero2);
+console.log(numero3);
+console.log(numero4);
