7627749841a2eb30db0c62b5aaf2ad761ae77035
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-07 09:50:50 -0600

    js

--- JS/06.js
+++ JS/06.js
@@ -4,3 +4,9 @@ let resultado;
 resultado = Math.PI;
 resultado = Math.round(2.8);
 console.log(resultado);
+// Redondea hacia arriba
+resultado = Math.ceil(2.1);
+console.log(resultado);
+// Redondea hacia abajo
+resultado = Math.floor(2.9);
+console.log(resultado);
