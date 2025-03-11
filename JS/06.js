e2fdf92a457fc490854c5fc1d968546747972992
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2025-03-11 12:15:24 -0600

    change js folder

--- JS/06.js
+++ JS/06.js
@@ -2,11 +2,21 @@
 
 let resultado;
 resultado = Math.PI;
-resultado = Math.round(2.8);
+resultado = Math.round(2.8);//3 Redondea
 console.log(resultado);
 // Redondea hacia arriba
-resultado = Math.ceil(2.1);
+resultado = Math.ceil(2.1);//3
 console.log(resultado);
 // Redondea hacia abajo
-resultado = Math.floor(2.9);
+resultado = Math.floor(2.9);//2
+console.log(resultado);
+
+/// Raiz cuadrada
+resultado = Math.sqrt(256);// 16
+console.log(resultado);
+
+resultado = Math.abs(-500); // Valor absoluto
+console.log(resultado);
+
+resultado = Math.min(3,5,1,12,4);
 console.log(resultado);
